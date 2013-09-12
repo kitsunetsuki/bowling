7dff514f86b78f6bf00b99233f37ebc51b9981dd
Bonnie <blmattson@gmail.com>
2013-09-12 11:18:32 -0500

    add working score logic

--- bowling.rb
+++ bowling.rb
@@ -11,15 +11,22 @@ class BowlingGame
   def score
     total_score = 0
     current_roll = 0
+
     while current_roll < @rolls.size - 1
       roll = @rolls[current_roll]
-      next_roll = @rolls[current_roll+1]
-      if roll + next_roll ==10
-        total_score += 10 + @rolls[current_roll + 2]
+      next_roll = @rolls[current_roll + 1]
+
+      if roll == 10
+        total_score += 10 + @rolls[current_roll + 1] + @rolls[current_roll + 2]
+        current_roll += 1
+      elsif roll + next_roll == 10
+        total_score = 10 + @rolls[current_roll + 2]
+        current_roll += 2 
       else
         total_score += roll +  next_roll
+        current_roll += 2
       end
-      current_roll += 2
     end
+    return total_score
   end
 end
