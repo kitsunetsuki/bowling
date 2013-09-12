fc937ba52f792c60b3b967c5243adacfa2bc25c5
Bonnie <blmattson@gmail.com>
2013-09-12 10:25:43 -0500

    add passing score and roll methods

--- bowling.rb
+++ bowling.rb
@@ -3,4 +3,11 @@ class BowlingGame
     def initialize
         @rolls = []
     end
+
+    def roll(pins)
+    end
+    
+    def score
+        0
+    end
 end
--- spec/bowling_spec.rb
+++ spec/bowling_spec.rb
@@ -2,19 +2,17 @@ require 'rubygems'
 require 'bundler/setup'
 require 'rspec'
 require_relative '../bowling'
-describe BowlingGame do 
+describe BowlingGame do
+  let(:game){BowlingGame.new} 
   describe '.initialize' do
     it 'sets  @rolls to an empty array' do
-        game = BowlingGame.new
         expect(game.rolls).to eq([])
     end
   end  
   describe '#score' do
     it 'scores the game as 0 when only gutter balls are thrown' do
-      game = BowlingGame.new
       20.times{game.roll(0)}
-
-      expect (game.score).to eq(0)
+      expect(game.score).to eq(0)
     end
   end
 
