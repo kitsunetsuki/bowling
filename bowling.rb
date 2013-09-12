9ab1649727e934fa716a36761164cbc962c68e73
Bonnie <blmattson@gmail.com>
2013-09-12 11:07:21 -0500

    add passing score test for spares, failing for strikes

--- bowling.rb
+++ bowling.rb
@@ -1,13 +1,25 @@
 class BowlingGame
-    attr_reader :rolls
-    def initialize
-        @rolls = []
-    end
+  attr_reader :rolls
+  def initialize
+    @rolls = []
+  end
 
-    def roll(pins)
+  def roll(pins)
+    @rolls.push(pins)
+  end
+
+  def score
+    total_score = 0
+    current_roll = 0
+    while current_roll < @rolls.size - 1
+      roll = @rolls[current_roll]
+      next_roll = @rolls[current_roll+1]
+      if roll + next_roll ==10
+        total_score += 10 + @rolls[current_roll + 2]
+      else
+        total_score += roll +  next_roll
+      end
+      current_roll += 2
     end
-    
-    def score
-        0
-    end
+  end
 end
--- spec/bowling_spec.rb
+++ spec/bowling_spec.rb
@@ -9,15 +9,38 @@ describe BowlingGame do
         expect(game.rolls).to eq([])
     end
   end  
+  
   describe '#score' do
-    it 'scores the game as 0 when only gutter balls are thrown' do
-      20.times{game.roll(0)}
-      expect(game.score).to eq(0)
+    context ' when only gutter balls are thrown' do
+      it 'scores the game as 0' do
+        20.times{game.roll(0)}
+        expect(game.score).to eq(0)
+      end
+    end 
+    context 'when one pin is knocked down on each roll' do
+      it 'scores the game as 20' do
+        20.times{game.roll(1)}
+        expect(game.score).to eq(20)
+      end  
     end
-    it 'scores the game as 20 when one pin is knocked down each roll' do
-      20.times{game.roll(1)}
-      expect(game.score).to eq(20)
-    end  
-  end
+    context 'when a spare is thrown' do
+      it 'records the frame score as 10 plus number of pins knocked down in the next roll' do
+        3.times{game.roll(5)}
+        17.times{game.roll(0)}
 
+        expect(game.score).to eq(20)
+      end
+    end
+    context'when a strike is thrown' do
+      it 'records the frome score as 10 plus the total number of pins knocked down in the next two frames' do
+        game.roll(10)
+        game.roll(7)
+        game.roll(1)
+        16.times{game.roll(0)}
+
+        expect(game.score).to eq(26)
+      end
+    end
+
+  end
 end
